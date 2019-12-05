6a645c3e0907015345b1c86e704685f0de31ce1f
schneems <richard.schneeman+foo@gmail.com>
2019-12-05 14:51:27 -0600

    Better output messaging

--- testcase.rb
+++ testcase.rb
@@ -6,7 +6,12 @@ connection_parameters = {
   connect_timeout: "15s",
 }
 
-conn = PG.connect(connection_parameters)
-conn.exec("SELECT 'test'") do |result|
-  puts "Succeeded"
+begin
+  conn = PG.connect(connection_parameters)
+  conn.exec("SELECT 'test'") do |result|
+    puts "🙀🙀🙀 PG can load which means libpg5.12.1 is not used"
+  end
+rescue => e
+  puts e.message
+  puts "👍👍👍 Success!!! An error is expected" if e.message.include?("invalid integer value")
 end
